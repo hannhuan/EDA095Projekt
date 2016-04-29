9e4d7b7fe41042f3f4db05608640e38ef5f5f7f4
AnnieLeonia <annie.onnered@gmail.com>
2016-04-29 10:56:03 +0200

    Added files via upload
    Uppdaterad GUI och fungerande chatt!

---
+++ SquadCode/src/update2/RefreshListner.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class RefreshListner implements ActionListener {
+
+	public RefreshListner(ProjectGUI projectGUI) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		
+	}
+
+}
