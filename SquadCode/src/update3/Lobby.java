5b6c5c3b0b57abac38c87e357406465d80482175
hannhuan <hannhuan@hotmail.com>
2016-05-04 14:08:21 +0200

    Added files via upload

--- SquadCode/src/update3/Lobby.java
+++ SquadCode/src/update3/Lobby.java
@@ -1,23 +1,32 @@
 package update3;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Lobby {
 	
+	private String name;
 	private List<Socket> people;
-	private List<HashMap <String, LobbyClass>> klasser;
+	private HashMap <String, LobbyClass> klasser;
 	
-	public Lobby(){
-		
+	public Lobby(String name){
+		this.name = name;
+		people = new ArrayList<Socket>();
+		klasser = new HashMap<String, LobbyClass>();
+	}
+	
+	public void newClass(String name, String content){
+		LobbyClass lclass = new LobbyClass(name, content);
+		klasser.put(lclass.getName(), lclass);
 	}
 	
 	public List<Socket> getPeople(){
 		return people;
 	}
 	
-	public List<HashMap <String, LobbyClass>> getClasses(){
+	public HashMap <String, LobbyClass> getClasses(){
 		return klasser;
 	}
 	
--- SquadCode/src/update3/LobbyClass.java
+++ SquadCode/src/update3/LobbyClass.java
@@ -1,13 +1,15 @@
 package update3;
 
 public class LobbyClass {
+	private String name;
 	private String content;
 	private boolean ifUpdate;
 	
 	
-	public LobbyClass(){
+	public LobbyClass(String name, String content){
+		this.name = name;
+		this.content = content;
 		ifUpdate = false;
-		
 	}
 	
 	public String update(){
@@ -21,6 +23,10 @@ public class LobbyClass {
 	public void submit (String content){
 		this.content = content;
 		ifUpdate = true;
+	}
+	
+	public String getName(){
+		return name;
 	}
 	
 	
